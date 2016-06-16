044633a89236fb7a6c671733f8be7d7905ded2c2
Calvin Claus <calvinclaus@me.com>
2016-06-17 00:11:24 +0200

    Cleaned up this.props usage and indented with 4 spaces

--- src/components/CalendarDay.js
+++ src/components/CalendarDay.js
@@ -1,39 +1,40 @@
 import React from 'react';
 import classnames from 'classnames';
 
-class CalendarDay extends React.Component {
-  render () {
-    const dayClasses = classnames({
-        'flexColumn': true,
-        'day': true,
-        'inactive': this.props.day.siblingMonth,
-        'today': this.props.isToday,
-    });
+export default class CalendarDay extends React.Component {
+    render () {
+        const { day, isToday, events, onClick } = this.props;
+        const dayClasses = classnames({
+            'flexColumn': true,
+            'day': true,
+            'inactive': day.siblingMonth,
+            'today': isToday,
+        });
 
-    return (
-      <div 
-        onClick={this.props.onClick.bind(null, this, this.props.day)}
-        className={dayClasses}>
-            <div className="inner-grid">
-                <div className="date">
-                    {this.props.day.day}
+        return (
+            <div 
+                onClick={onClick.bind(null, this, day)}
+                className={dayClasses}>
+                <div className="inner-grid">
+                    <div className="date">
+                        {day.day}
+                    </div>
+                    {events}
                 </div>
-                {this.props.events}
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 CalendarDay.propTypes = {
-  day: React.PropTypes.object.isRequired,
-  isToday: React.PropTypes.bool,
-  events: React.PropTypes.array,
-  onClick: React.PropTypes.func,
+    day: React.PropTypes.object.isRequired,
+    isToday: React.PropTypes.bool,
+    events: React.PropTypes.array,
+    onClick: React.PropTypes.func,
 };
 
 CalendarDay.defaultProps = {
-  onClick: () => {},
+    onClick: () => {},
 }
 
 export default CalendarDay;
